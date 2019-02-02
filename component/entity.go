4e0a27b07d763ef1565bc66e2e30d886a97e98ed
Dominik Braun <36575275+DominikBraun@users.noreply.github.com>
2019-02-02 18:22:50 +0100

    Add files via upload

--- component/entity.go
+++ component/entity.go
@@ -1,3 +1,8 @@
 package component
 
-type Entity struct {}
+// Alle Typen aus dem Package components stellen eine Komposition dar, die auf
+// Entity basiert. Folglich kann Entity in anderen Packages - beispielsweise in
+// database - stellvertretend für alle anderen Components verwendet werden.
+type Entity struct {
+	ID string // Die ID als String für beispielsweise transaction.Tx
+}
