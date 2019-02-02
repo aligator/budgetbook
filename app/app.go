b1db00ae0f3182d75f464f0bdcd5f8f7740fd8cf
Dominik Braun <36575275+DominikBraun@users.noreply.github.com>
2019-02-02 01:06:47 +0100

    Add files via upload

--- app/app.go
+++ app/app.go
@@ -6,12 +6,19 @@ import (
 	"github.com/spf13/viper"
 )
 
+// Repräsentiert eine ausführbare Instanz der Anwendung. Neben der Datenbank
+// und der Konfiguration wird auch ein Haupt-Command geladen, der beim Start
+// ausgeführt wird. An diesen Haupt-Command werden mit bindCommands() die
+// verschiedenen Subcommands angehängt.
 type app struct {
-	DB      persistence.Database
-	RootCmd *cobra.Command
-	Config  *viper.Viper
+	DB      persistence.Database // Implementierung von persistence.Database.
+	RootCmd *cobra.Command       // Haupt-Command, an dem die Subcommands hängen.
+	Config  *viper.Viper         // Library zum Zugriff auf Config-Werte.
 }
 
+// Erzeugt eine neue Instanz der Anwendung. Hierbei wird eine leere Instanz einer
+// beliebigen persistence.Database-Implementierung angelegt. Als Haupt-Command
+// wird der im Paket hinterlegte RootCmd herangezogen.
 func New() *app {
 	bolt := &persistence.Bolt{}
 	if err := bolt.Load(); err != nil {
@@ -23,6 +30,9 @@ func New() *app {
 	}
 }
 
+// Führt die Instanz der Anwendung aus. Sollten zu diesem Zeitpunkt noch keine
+// Subcommands an den Haupt-Command gehängtn worden sein, wird dies nachgeholt.
+// Anschließend wird der Haupt-Command der App ausgeführt.
 func (a *app) Run() {
 	if !a.RootCmd.HasSubCommands() {
 		a.bindCommands(Cmds)
@@ -30,6 +40,9 @@ func (a *app) Run() {
 	a.RootCmd.Execute()
 }
 
+// Hängt ein Slice von Commands an den Haupt-Command der App. Diese Funktion
+// kann auch genutzt werden, um Commands nachträglich an den Haupt-Command
+// zu binden.
 func (a *app) bindCommands(cmds []*cobra.Command) {
 	for i, _ := range cmds {
 		a.RootCmd.AddCommand(cmds[i])
