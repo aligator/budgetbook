6997573b0f3f1f20d0b2521a7511336c1cd5ba6a
Dominik Braun <36575275+DominikBraun@users.noreply.github.com>
2019-02-04 21:39:42 +0100

    Delete entity.go

--- component/entity.go
+++
@@ -1,8 +0,0 @@
-package component
-
-// Alle Typen aus dem Package components stellen eine Komposition dar, die auf
-// Entity basiert. Folglich kann Entity in anderen Packages - beispielsweise in
-// database - stellvertretend für alle anderen Components verwendet werden.
-type Entity struct {
-	ID string // Die ID als String für beispielsweise transaction.Tx
-}
