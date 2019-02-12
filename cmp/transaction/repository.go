ada52439aaa14071595f4ebbab6aa38a18ba8fc1
Dominik Braun <36575275+dominikbraun@users.noreply.github.com>
2019-02-12 12:50:59 +0100

    Add files via upload

--- cmp/transaction/repository.go
+++ cmp/transaction/repository.go
@@ -4,22 +4,27 @@ import "budgetBook/cmp"
 
 type Repository struct {}
 
+// Implements cmp.Repository.Find().
 func (r *Repository) Find(id string) *cmp.Entity {
 	return nil
 }
 
+// Implements cmp.Repository.FindAll().
 func (r *Repository) FindAll() []*cmp.Entity {
 	return nil
 }
 
+// Implements cmp.Repository.Insert().
 func (r *Repository) Insert(e *cmp.Entity) error {
 	return nil
 }
 
+// Implements cmp.Repository.Update().
 func (r *Repository) Update(e *cmp.Entity) error {
 	return nil
 }
 
+// Implements cmp.Repository.Delete().
 func (r *Repository) Delete(e *cmp.Entity) error {
 	return nil
 }
