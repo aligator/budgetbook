fb5fd157c7ca217a14b8f18a33ced6573a042c9f
Dominik Braun <36575275+dominikbraun@users.noreply.github.com>
2019-02-12 12:27:47 +0100

    Add files via upload

--- cmp/entity.go
+++ cmp/entity.go
@@ -22,3 +22,19 @@ type Entity interface {
 	// Fills an empty entity with values from JSON.
 	UnmarshalJSON(b []byte) error
 }
+
+// The Repository interface makes sure that every repository for the cmp
+// types implement the essential methods listed below.
+type Repository interface {
+	// Finds an entity by id.
+	Find(id string) *Entity
+	// Returns all entities of the table the repository is responsible for.
+	FindAll() []*Entity
+	// Inserts a given entity into the corresponding table. If the entity
+	// already exists, an error will be returned.
+	Insert(e *Entity) error
+	// Updates a given entity. If it doesn't exist yet, it will be inserted.
+	Update(e *Entity) error
+	// Deletes a given entity from its containing table.
+	Delete(e *Entity) error
+}
