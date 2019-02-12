d931d52937b78bc2459dff4c4d74bab7c54ef5be
Dominik Braun <36575275+dominikbraun@users.noreply.github.com>
2019-02-12 12:42:09 +0100

    Add files via upload

--- app/commands.go
+++ app/commands.go
@@ -1,6 +1,9 @@
 package app
 
-import "budgetBook/intc"
+import (
+	"budgetBook/intc"
+	"fmt"
+)
 
 // Builds the entire command set including the root command and returns
 // interchangeable, general purpose commands that can be transformed by
@@ -16,30 +19,17 @@ func buildCommandSet() (*intc.Command, []*intc.Command) {
 		Use:  "add-cat",
 		Help: ``,
 		Options: []*intc.Flag{
-			{
-				Name:      "name",
-				Shorthand: "n",
-				Help:      ``,
-				DefVal:    "",
-			},
-			{
-				Name:      "inc",
-				Shorthand: "inc",
-				Help:      ``,
-				DefVal:    "",
-			},
-			{
-				Name:      "capped",
-				Shorthand: "c",
-				Help:      ``,
-				DefVal:    "",
-			},
-			{
-				Name:      "budget",
-				Shorthand: "b",
-				Help:      ``,
-				DefVal:    "",
-			},
+			{Name: "name", Shorthand: "n", Help: ``, DefVal: ""},
+			{Name: "inc", Shorthand: "inc", Help: ``, DefVal: ""},
+			{Name: "capped", Shorthand: "c", Help: ``, DefVal: ""},
+			{Name: "budget", Shorthand: "b", Help: ``, DefVal: ""},
+		},
+		Run: func(options []*intc.Flag) error {
+			// Testing flag store values.
+			for _, opt := range options {
+				fmt.Println(opt.Name, ":", opt.Store)
+			}
+			return nil
 		},
 	}
 	cmdSet := []*intc.Command{addCategory}
