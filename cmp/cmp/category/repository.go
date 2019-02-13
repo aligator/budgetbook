63af7b32efa5960c6fe73500d1c34731e802f79a
Dominik Braun <36575275+dominikbraun@users.noreply.github.com>
2019-02-13 21:55:52 +0100

    Add files via upload

---
+++ cmp/cmp/category/repository.go
@@ -0,0 +1,55 @@
+package category
+
+import (
+	"budgetBook/cmp"
+	"budgetBook/persist"
+	"github.com/pkg/errors"
+)
+
+type repository struct {
+	db persist.Database
+}
+
+func NewRepository() *repository {
+	r := &repository{
+		db: &persist.Bolt{},
+	}
+	_ = r.db.Setup()
+	return r
+}
+
+// Implements cmp.repository.Find().
+func (r *repository) Find(id string) cmp.Entity {
+	return nil
+}
+
+// Implements cmp.repository.FindAll().
+func (r *repository) FindAll() []cmp.Entity {
+	return nil
+}
+
+// Implements cmp.repository.Insert().
+func (r *repository) Insert(e cmp.Entity) error {
+	id := e.ID()
+	if bytes, err := e.(*Cat).MarshalJSON(); err != nil {
+		return r.db.Insert([]byte(id), bytes, persist.CatTable)
+	}
+	return nil
+}
+
+// Implements cmp.repository.Update().
+func (r *repository) Update(e cmp.Entity) error {
+	return nil
+}
+
+// Implements cmp.repository.Delete().
+func (r *repository) Delete(e cmp.Entity) error {
+	return nil
+}
+
+func toCat(e cmp.Entity) (*Cat, error) {
+	if c, ok := e.(*Cat); ok {
+		return c, nil
+	}
+	return nil, errors.New("given entity is not an instance of Cat")
+}
