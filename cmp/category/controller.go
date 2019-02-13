6cdfbc0a3cf870aa5057cb18c21c5459c95961c2
Dominik Braun <36575275+dominikbraun@users.noreply.github.com>
2019-02-13 21:01:56 +0100

    Add files via upload

---
+++ cmp/category/controller.go
@@ -0,0 +1,12 @@
+package category
+
+import (
+	"budgetBook/intc"
+	"fmt"
+)
+
+func Create(cmd *intc.Command) error {
+	fmt.Println("Creating new Category")
+
+	return nil
+}
--- cmp/category/repository.go
+++ cmp/category/repository.go
@@ -1,30 +1,55 @@
 package category
 
-import "budgetBook/cmp"
+import (
+	"budgetBook/cmp"
+	"budgetBook/persist"
+	"github.com/pkg/errors"
+)
 
-type Repository struct {}
+type repository struct {
+	DB persist.Database
+}
 
-// Implements cmp.Repository.Find().
-func (r *Repository) Find(id string) *cmp.Entity {
+func NewRepository() *repository {
+	r := &repository{
+		DB: &persist.Bolt{},
+	}
+	_ = r.DB.Setup()
+	return r
+}
+
+// Implements cmp.repository.Find().
+func (r *repository) Find(id string) *cmp.Entity {
 	return nil
 }
 
-// Implements cmp.Repository.FindAll().
-func (r *Repository) FindAll() []*cmp.Entity {
+// Implements cmp.repository.FindAll().
+func (r *repository) FindAll() []*cmp.Entity {
 	return nil
 }
 
-// Implements cmp.Repository.Insert().
-func (r *Repository) Insert(e *cmp.Entity) error {
+// Implements cmp.repository.Insert().
+func (r *repository) Insert(e *cmp.Entity) error {
+	id := (*e).ID()
+	if bytes, err := (*e).(*Cat).MarshalJSON(); err != nil {
+		return r.DB.Insert([]byte(id), bytes, persist.CatTable)
+	}
 	return nil
 }
 
-// Implements cmp.Repository.Update().
-func (r *Repository) Update(e *cmp.Entity) error {
+// Implements cmp.repository.Update().
+func (r *repository) Update(e *cmp.Entity) error {
 	return nil
 }
 
-// Implements cmp.Repository.Delete().
-func (r *Repository) Delete(e *cmp.Entity) error {
+// Implements cmp.repository.Delete().
+func (r *repository) Delete(e *cmp.Entity) error {
 	return nil
+}
+
+func toCat(e *cmp.Entity) (*Cat, error) {
+	if c, ok := (*e).(*Cat); ok {
+		return c, nil
+	}
+	return nil, errors.New("given entity is not an instance of Cat")
 }
