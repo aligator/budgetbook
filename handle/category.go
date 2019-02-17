845848184f1ba4742b250e6e9466e18e85a5c8c8
Dominik Braun <36575275+dominikbraun@users.noreply.github.com>
2019-02-17 23:53:30 +0100

    Add files via upload

--- handle/category.go
+++ handle/category.go
@@ -8,7 +8,8 @@ import (
 )
 
 type catController struct {
-	db persist.Database
+	db    persist.Database
+	table string
 }
 
 func (c *catController) DAO() persist.Database {
@@ -21,9 +22,10 @@ func (c *catController) Create(cmd *intc.Command) error {
 		budget = 0
 	}
 	cat := category.New(cmd.P("name"), cmd.Opt("is-inc"), cmd.Opt("is-capped"), budget)
-	_, err = cat.MarshalJSON()
+	bytes, err := cat.MarshalJSON()
 	if err != nil {
 		return err
 	}
-	return nil
+	id := []byte(cat.ID())
+	return c.db.Insert(id, bytes, c.table)
 }
