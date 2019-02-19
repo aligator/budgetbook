58caa24c53a87a1efff04df6521d77b5ec0e8155
Dominik Braun <36575275+dominikbraun@users.noreply.github.com>
2019-02-19 22:01:39 +0100

    Add files via upload

--- intc/command.go
+++ intc/command.go
@@ -1,10 +1,17 @@
 package intc
 
 import (
-	"budgetBookArch/str"
+	"budgetBookArch/conf"
 	"errors"
 )
 
+// In general, intc.Command is used to depict any feature of the application
+// and is general purpose, package-wide interchangeable command that also
+// serves as a vehicle for parsed data.
+//
+// Command represents a certain functionality of the application. It does not
+// only provide information about the technical CLI command itself, but also
+// defines which data (see Params) is required to handle it appropriately.
 type Command struct {
 	Use     string
 	Help    string
@@ -13,32 +20,39 @@ type Command struct {
 	Run     func(cmd *Command) error
 }
 
+// Adds a new param to a given command. Since duplicate param names aren't
+// valid, an error will be returned if the param name already exists.
 func (cmd *Command) AddParam(p *Param) error {
 	if cmd.Params == nil {
 		cmd.Params = make(map[string]*Param)
 	}
 	for _, param := range cmd.Params {
 		if param.Name == p.Name {
-			return errors.New(cfg.CmdParamAlreadyExists)
+			return errors.New(conf.CmdParamAlreadyExists)
 		}
 	}
 	cmd.Params[p.Name] = p
 	return nil
 }
 
+// Adds a new option to a given command. Since duplicate option names aren't
+// valid, an error will be returned if the option name already exists.
 func (cmd *Command) AddOption(o *Option) error {
 	if cmd.Options == nil {
 		cmd.Options = make(map[string]*Option)
 	}
 	for _, opt := range cmd.Options {
 		if opt.Name == o.Name {
-			return errors.New(cfg.CmdOptionAlreadyExists)
+			return errors.New(conf.CmdOptionAlreadyExists)
 		}
 	}
 	cmd.Options[o.Name] = o
 	return nil
 }
 
+// Returns the value of a parameter with the specified key by de-referencing
+// the stored pointer. In case the key does not exist in the map of parameters,
+// the zero value will be returned.
 func (cmd *Command) P(key string) string {
 	if p, ok := cmd.Params[key]; ok {
 		return *p.Val
@@ -46,6 +60,9 @@ func (cmd *Command) P(key string) string {
 	return ""
 }
 
+// Returns the value of a option with the specified key by de-referencing
+// the stored pointer. In case the key does not exist in the map of options,
+// the zero value will be returned.
 func (cmd *Command) Opt(key string) bool {
 	if opt, ok := cmd.Options[key]; ok {
 		return *opt.Val
