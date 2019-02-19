1e13e435afe31e2165a7e514928c95e8d888b1c7
Dominik Braun <36575275+dominikbraun@users.noreply.github.com>
2019-02-19 18:12:49 +0100

    Add files via upload

--- conf/conf.go
+++ conf/conf.go
@@ -3,8 +3,7 @@ package conf
 // These constants allow the strings to be accessed easily from other packages.
 // However, all messages will be provided by a dedicated config library soon.
 const (
-	// Command-associated error messages. They are mainly used by the CLI types
-	// which convert and use the interchangeable commands (see cli/cli.go).
+	// Command-associated error messages.
 	CmdTypeAssertionFailed = "assertion to CLI library's command type failed"
 	CmdNotFoundInCtrSet    = "command not found in CLI library's container set"
 	CmdParamAlreadyExists  = "specified param already exists for the command"
